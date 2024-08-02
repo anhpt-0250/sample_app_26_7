2ff21f45eadd4a7350d74e92783ccf56431753e2
pham.tuan.anhc <tuananhchums23@gmail.com>
2024-08-02 14:30:13 +0700

    [Naitei SJP] Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   ATTRIBUTES_PERMITTED = %i(name email password password_confirmation).freeze
 
   before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true,
                    length: {maximum: Settings.degit.length_name_max}
@@ -43,12 +44,28 @@ class User < ApplicationRecord
     update_column :remember_digest, nil
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = public_send "#{attribute}_digest"
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
   end
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
